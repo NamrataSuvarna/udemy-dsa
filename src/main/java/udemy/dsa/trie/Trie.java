18a7c779c1fe69e7e0766b7650ecfb0eea21dbd1
Namrata Suvarna Cherodian <78306435+NamrataSuvarnaCherodian@users.noreply.github.com>
2023-10-24 22:16:06 +0530

    Add files via upload

--- src/main/java/udemy/dsa/trie/Trie.java
+++ src/main/java/udemy/dsa/trie/Trie.java
@@ -8,4 +8,74 @@ public class Trie {
 		root = new TrieNode();
 	}
 
+	public void delete(String word) {
+		if (search(word)) {
+			delete(root, word, 0);
+		}
+	}
+
+	private boolean delete(TrieNode trieNode, String word, int index) {
+		Character character = word.charAt(index);
+		TrieNode currentNode = trieNode.children.get(character);
+		boolean canThisNodeBeDeleted;
+		if (currentNode.children.size() > 1) {
+			delete(currentNode, word, index++);
+			return false;
+		}
+		if (index == word.length() - 1) {
+			if (currentNode.children.size() >= 1) {
+				currentNode.endOfString = false;
+				return false;
+			} else {
+				currentNode.children.remove(character);
+				return true;
+			}
+		}
+		if (currentNode.endOfString == true) {
+			delete(currentNode, word, index++);
+			return false;
+		}
+		canThisNodeBeDeleted = delete(currentNode, word, index++);
+		if (canThisNodeBeDeleted) {
+			currentNode.children.remove(character);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void insert(String word) {
+		TrieNode currentNode = root;
+		for (int i = 0; i < word.length(); i++) {
+			Character character = word.charAt(i);
+			TrieNode trieNode = currentNode.children.get(character);
+			if (trieNode == null) {
+				trieNode = new TrieNode();
+				currentNode.children.put(character, trieNode);
+			}
+			currentNode = trieNode;
+		}
+		currentNode.endOfString = true;
+		System.out.println("Successfully inserted " + word + " into trie.");
+	}
+
+	public boolean search(String word) {
+		TrieNode currentNode = root;
+		for (int i = 0; i < word.length(); i++) {
+			Character character = word.charAt(i);
+			TrieNode trieNode = currentNode.children.get(character);
+			if (trieNode == null) {
+				System.out.println(word + " does not exist in trie.");
+				return false;
+			}
+			currentNode = trieNode;
+		}
+		if (!currentNode.endOfString) {
+			System.out.println(word + " itself does not exist and is a prefix of another word in the trie.");
+			return false;
+		}
+		System.out.println(word + " exists in trie.");
+		return true;
+	}
+
 }
--- src/main/java/udemy/dsa/trie/TrieMain.java
+++ src/main/java/udemy/dsa/trie/TrieMain.java
@@ -7,6 +7,18 @@ public class TrieMain {
 		// Time & Space Complexity - O(1)
 		Trie trie = new Trie();
 
+		// Time & Space Complexity - O(M) -> M = length of word
+		trie.insert("API");
+		trie.insert("APIS");
+
+		// Time & Space Complexity - O(M) -> M = length of word
+		trie.search("API");
+		trie.search("AP");
+		trie.search("APS");
+
+		trie.delete("API");
+		trie.delete("AP");
+
 	}
 
 }
